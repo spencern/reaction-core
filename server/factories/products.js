082741108622f21f17bfa196308001681e40103d
Spencer Norman <spencern@gmail.com>
2015-09-14 10:51:42 -0600

    Update product faker to prep for update to 0.7.0

--- server/factories/products.js
+++ server/factories/products.js
@@ -7,8 +7,9 @@ Fake.reactionMetaField = function(params) {
   };
 };
 
-Fake.reactionProductVariant = function() {
-  return {
+Fake.reactionProductVariant = function(params) {
+  params = params || {};
+  defaultValues = {
     _id: Random.id(),
     compareAtPrice: _.random(0, 1000),
     weight: _.random(0, 10),
@@ -27,7 +28,24 @@ Fake.reactionProductVariant = function() {
       Fake.reactionMetaField({key: 'twitter', scope: 'socialMessages'})
     ]
   };
+  return _.extend(defaultValues, params);
 };
+
+Fake.reactionProductInventoryVariant = function(params) {
+  params = params || {};
+  defaultValues = {
+    _id: Random.id(),
+    barcode: _.random(100000,999999), //Random 6 Digit Barcode
+    parentId: Random.id(),
+    type: 'inventory'
+  };
+  return _.extend(defaultValues, params);
+};
+
+// Setup some fake ids to be shared among parent and child variants.
+sharedId1 = Random.id();
+sharedId2 = Random.id();
+sharedId3 = Random.id();
 
 Factory.define('product', ReactionCore.Collections.Products, {
   shopId: Factory.get('shop'),
@@ -56,6 +74,62 @@ Factory.define('product', ReactionCore.Collections.Products, {
   createdAt: new Date(),
   updatedAt: new Date()
 });
+
+Factory.define('productWithChildVariants', ReactionCore.Collections.Products,
+  Factory.extend('product', {
+    variants: function() {
+      return [
+        // Parent Variant - variants[0]
+        Fake.reactionProductVariant({_id: sharedId1}),
+        // Child Variants - variants[1..8]
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        // Child Variant w/ Children variants[8]
+        Fake.reactionProductVariant({
+          _id: sharedId2,
+          parentId: sharedId1
+        }),
+        // Grandchildren variants - variants[9..11]
+        Fake.reactionProductVariant({parentId: sharedId2}),
+        Fake.reactionProductVariant({parentId: sharedId2}),
+        Fake.reactionProductVariant({parentId: sharedId2})
+      ];
+    }
+  }));
+
+Factory.define('productWithInventoryVariants', ReactionCore.Collections.Products,
+  Factory.extend('product', {
+    variants: function() {
+      return [
+        // Parent Variant - variants[0]
+        Fake.reactionProductVariant({_id: sharedId1}),
+        // Child Variants - variants[1..2]
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        Fake.reactionProductVariant({parentId: sharedId1}),
+        // Child Variant w/ inventory - variants[3]
+        Fake.reactionProductVariant({
+          _id: sharedId2,
+          parentId: sharedId1
+        }),
+        // inventory variants - variants[4..13]
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2}),
+        Fake.reactionProductInventoryVariant({parentId: sharedId2})
+      ];
+    }
+  }));
 
 Factory.define('tag', ReactionCore.Collections.Tags, {
   name: Fake.word,
